fdcdef67c1095145d59335554b1f5c468efc7fc4
Maire Chew <mairechew@gmail.com>
2017-10-13 14:52:37 -0600

    working

--- src/Card.js
+++ src/Card.js
@@ -5,13 +5,38 @@ import React from 'react';
 class Card extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { username: props.username };
+    console.log()
+// keeps local state
+    this.state = { user: {},
+                   username: props.username,
+                   message: 'Ready, enter username' };
   }
 
+  //used to make AJAX calls and set state
+    componentDidMount() {
+      if (this.state.username && this.state.username.length >= 3) {
+        fetch('https://api.github.com/users/' + this.state.username)
+        // get data from AJAX call & make JSON object
+          .then(res => res.json())
+        // set state to data from AJAX call
+          .then(user => this.setState({...this.state, user: user})) //this = card component
+          .catch( err => {
+            this.setState({...this.state, message: err.message})
+          })
+      }   else {
+            this.setState({...this.state, message: 'Ready, enter username'})
+      }
+    }
+
   render() {
+    // initial message
+    if (this.state.message) {
+      return <div className="statusMessage">{this.state.message}></div>
+    }
     return (
       <div className="Card">
-        <h3> {this.state.username}</h3>
+        <img src={this.state.user.avatar_url}/>
+        <h3> {this.state.user.name}</h3>
       </div>
     );
   }
--- src/Search.js
+++ src/Search.js
@@ -5,20 +5,22 @@ import React from 'react';
 class Searchform extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { username: '' };
+    this.state = { searchUser: '' };
     this.onSearch = props.onSearch;
   }
+
+
 
   render() {
     return (
       <div className="Search">
         <h3>Enter github</h3>
         <input
-          onChange={e => {
-            this.setState({ username: e.target.value });
+          onChange={(event) => {
+            this.setState({ searchUser: event.target.value });
           }}
         />
-        <button onClick={e => this.onSearch(this.state.username)}>Search</button>
+        <button onClick={() => this.onSearch(this.state.searchUser)}>Search</button>
         <div />
       </div>
     );
